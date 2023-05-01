50f07ddcb25d46ffa76f0d60b265cbfa6b54c876
Mumtaz Ahmad <85639625+ahmadmum@users.noreply.github.com>
2023-05-01 20:24:38 +0200

    Add files via upload

---
+++ Amazon Stock Price.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  1 20:14:20 2023
+
+@author: mumta
+"""
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+
+
+
+
+amazon_data = pd.read_csv('amazon_stock_price.csv')
